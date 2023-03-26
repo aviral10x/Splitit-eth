1128fad47108b70e2df0a1742da46e3c515413ca
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2023-03-26 21:30:51 +0530

    Update hardhat.config.js

--- Splitit/hardhat.config.js
+++ Splitit/hardhat.config.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
-
+const optimismGoerliUrl = process.env.ALCHEMY_API_KEY
+  ? `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+  : process.env.OPTIMISM_GOERLI_URL;
+  
 module.exports = {
-    solidity: "0.8.4",
-    networks: {
+  solidity: "0.8.4",
+  networks: {
     scrollAlpha: {
       url: "https://alpha-rpc.scroll.io/l2" || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    
+    "optimism-goerli": {
+      url: optimismGoerliUrl,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
   },
 };
 
-const optimismGoerliUrl = process.env.ALCHEMY_API_KEY
-  ? `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-  : process.env.OPTIMISM_GOERLI_URL;
+
