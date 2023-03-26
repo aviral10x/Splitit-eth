3af3309e993e6a8452752822576537367734a610
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2023-03-26 21:29:08 +0530

    Update hardhat.config.js

--- Splitit/hardhat.config.js
+++ Splitit/hardhat.config.js
@@ -13,4 +13,6 @@ module.exports = {
   },
 };
 
-
+const optimismGoerliUrl = process.env.ALCHEMY_API_KEY
+  ? `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+  : process.env.OPTIMISM_GOERLI_URL;
